99875e523f5fd6a698e0124c31cba6094ffcba21
Nguyễn Ngọc Tuấn <91770556+NgocTuanedit@users.noreply.github.com>
2021-10-02 12:07:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Hi Mập",
     introTitle: 'Quyên Mập',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Nhân dịp mừng thọ lần thứ 20 của Quyên
+    Xin chúc Mập thêm xinh gái,
+    học giỏi, đầy năng lượng tích cực
+    Và sớm có được người yêu như ý`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Quà tặng tuy nhỏ nhưng đầy tình cảm 🥰',
+    desc: 'Quyên thích món quà tinh thần như này đúng hông? ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'U là trời, Bạn đúng là hảo chí cốt <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/ngoctuan2k1.binhdinh' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
